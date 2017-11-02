c0c6213ae2521ac91ce6dad08e620bd212f0e22e
jieun <poiujkl9875@hanmail.net>
2017-11-02 20:06:07 +0900

    UI design started, map added, markder added

---
+++ app/src/main/java/com/example/jieun/project2/ThingsToDo.java
@@ -0,0 +1,13 @@
+package com.example.jieun.project2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThingsToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_things_to_do);
+    }
+}
